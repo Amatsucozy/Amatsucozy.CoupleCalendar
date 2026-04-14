e372b1d60c1d3044bf322127c51659fea34cb795
Amatsucozy <amatsucozy@gmail.com>
2026-04-15 01:06:58 +0700

    fix deploy args

--- couple-calendar-angular/src/environments/environment.prod.ts
+++ couple-calendar-angular/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   useMockApi: false,
-  auth0Domain: 'amagumo-dev.jp.auth0.com',
-  auth0ClientId: 'GPwqvAMG9QGCYQu1P7IZ9Dz81HNZZUjE',
+  auth0Domain: 'couple-calendar.jp.auth0.com',
+  auth0ClientId: 'qUTwWpBsnG71llEnsx510RDc0AGPwipR',
   auth0Audience: 'couple-calendar-api',
   apiBaseUrl: 'https://api.couple-calendar.amatsucozy.com'
 };
