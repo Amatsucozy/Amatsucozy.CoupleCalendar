a77c62b8ea38c140810ca8a41add4e82d52a0b3c
Amatsucozy <amatsucozy@gmail.com>
2026-04-15 00:38:06 +0700

    fix prod domain

--- couple-calendar-angular/src/environments/environment.prod.ts
+++ couple-calendar-angular/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   auth0Domain: 'amagumo-dev.jp.auth0.com',
   auth0ClientId: 'GPwqvAMG9QGCYQu1P7IZ9Dz81HNZZUjE',
   auth0Audience: 'couple-calendar-api',
-  apiBaseUrl: 'https://localhost:7132'
+  apiBaseUrl: 'https://api.couple-calendar.amatsucozy.com'
 };
